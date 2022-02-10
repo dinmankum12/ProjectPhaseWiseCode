078bde7786f9a434b302c3f9b869d0ff0a2dbf23
Dinesh Kumar Mandal <96870706+dinmankum12@users.noreply.github.com>
2022-02-10 07:26:19 +0000

    Add files via upload

---
+++ PhaseThreeCode/lession2/Assessment-REST/src/main/java/com/dinesh/AssessmentRestApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentRestApplication.class, args);
+	}
+
+}
