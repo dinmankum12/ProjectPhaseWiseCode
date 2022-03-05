23c0a71be07bf98253af73be1d017b5662d00ff6
Dinesh Kumar Mandal <96870706+dinmankum12@users.noreply.github.com>
2022-03-05 13:49:41 +0530

    Add files via upload

---
+++ PhaseFive/main-project-lession1/Phase5lession1/src/main/java/com/dinesh/Phase5lession1/App.java
@@ -0,0 +1,13 @@
+package com.dinesh.Phase5lession1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
